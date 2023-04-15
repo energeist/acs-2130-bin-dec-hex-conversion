9e0394e98e4f3b5a4b3bdb6aed800d562ace715b
Mark Rattle <mark.rattle@gmail.com>
2023-04-14 22:35:23 -0400

    done with stretch challenge

---
+++ bin-dec-hex-conversion.py
@@ -0,0 +1,90 @@
+def binary_to_decimal(binary_str):
+    """returns the decimal number for the given binary digits"""
+    binary_str = str(binary_str)
+    n = len(str(binary_str))
+    i = 0
+    numbers = []
+    while i < n:
+        numbers.append(int(binary_str[-(n-i)])*(2**(n-i-1)))
+        i += 1
+    return sum(numbers)
+
+assert binary_to_decimal('1011') == 11
+assert binary_to_decimal('00000') == 0
+assert binary_to_decimal('00001') == 1
+assert binary_to_decimal('11111') == 31
+
+
+def decimal_to_binary(decimal_num):
+    """the binary representation of the given decimal number"""
+    #take my decimal number
+    #keep dividing by base (2): loop?
+    #I want to record the remainders some how: maybe store in a list?
+    #I want to stop when my thing I'm dividing by is less than the base: conditional or loop stopper: while loop?
+
+    #special case for 0 and 1
+
+    if decimal_num == 0 or decimal_num == 1:
+        return str(decimal_num)
+
+    remainders = []
+
+    while decimal_num > 0:
+
+        remainder = decimal_num % 2
+        remainders.append(str(remainder))
+        print("Remainder: ", remainder)
+
+        decimal_num = decimal_num // 2
+        print("Decimal: ", decimal_num)
+    
+    print("".join(remainders))
+    return "".join(remainders)[::-1]
+  
+assert decimal_to_binary(0) == '0'
+# #result of our code    expected result
+assert decimal_to_binary(1) == '1'
+assert decimal_to_binary(2) == '10'
+assert decimal_to_binary(55) == '110111'
+assert decimal_to_binary(389) == '110000101'
+
+#STRETCH: can you use what you have written so far to create hex_to_decimal() and decimal_to_hex() functions?
+
+my_string = 'abcd'
+
+def hex_to_decimal(hex_string):
+    # I'm avoiding just using int(__, 16) on the whole number because that seemed outside of the spirit of the exercise
+    """returns a decimal number from a given hexadecimal number"""
+    i = 0
+    result = 0
+    
+    for _ in str(hex_string[::-1]):
+        # using builtin python conversion to avoid an unneccessarily long 
+        # and pointless case switch statement to convert ABCDEF to dec equivalent digits
+        result += int(_, 16) * 16 ** i
+        i += 1
+    return result
+
+assert hex_to_decimal('abcd') == 43981
+assert hex_to_decimal('abcdef') == 11259375
+assert hex_to_decimal('abc00def') == 2881490415
+assert hex_to_decimal('a1b2c3') == 10597059
+
+def decimal_to_hex(decimal_num):
+    """returns a hexadecimal number from a given decimal number"""
+    # Again, I'm avoiding just using hex() on the whole string because that seemed outside of the spirit of the exercise
+    hex_digits = []
+    while decimal_num > 0:
+        # using a hex() builtin because the case switch statement to convert 10 11 12 13 14 15
+        # to a b c d e f is long and unnecessary again 
+        hex_digits.append(hex(decimal_num % 16)[-1])
+        decimal_num = decimal_num // 16
+    return "".join(hex_digits[::-1])
+
+assert decimal_to_hex(43981) == 'abcd'
+assert decimal_to_hex(11259375) == 'abcdef'
+assert decimal_to_hex(2881490415) == 'abc00def'
+assert decimal_to_hex(10597059) == 'a1b2c3'
+
+        
+    
